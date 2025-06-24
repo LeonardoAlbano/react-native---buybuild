1497bd646c1f96bd2e9e5e5a010a67e89a0cd6b3
leonardoalbano <leonardoalbanom@yahoo.com.br>
2025-06-24 06:21:24 -0300

    create: components Input, and ajust props for Button

--- src/app/Home/index.tsx
+++ src/app/Home/index.tsx
@@ -1,13 +1,19 @@
 import { Image, Text, View } from "react-native";
 import { styles } from "./styles";
 import { Button } from "@/components/Button";
+import { Input } from "@/components/Input";
 
 export function Home() {
   return (
     <View style={styles.container}>
       <Image source={require("@/assets/logo.png")} style={styles.logo} />
 
-      <Button />
+      <View style={styles.form}>
+        <Input placeholder="Salve familia" />
+        <Button title="Adicionar" />
+      </View>
+
+      <View style={styles.content}></View>
     </View>
   );
 }
